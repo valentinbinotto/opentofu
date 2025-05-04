46e7433ef8b8d7940b8f35eb5c21ed92f033c614
Valentin Binotto <git.8c400bab53f220f13efe0112c1ea2f96@v434project.com>
2025-05-04 16:33:57 +0000

    add os-cloud deploy for networking

--- os-deploy/network-0001/main.tf
+++ os-deploy/network-0001/main.tf
@@ -26,36 +26,17 @@ variable "network-0001-name" {
 
 variable "network-0001-cidr-ipv4" {
   type = string
-  description = "network-0001 IPv4 CIDR"
+  description = "network-0001 IPv4 CIDR (/16)"
 }
 variable "network-0001-cidr-ipv6" {
   type = string
-  description = "network-0001 IPv6 CIDR"
+  description = "network-0001 IPv6 CIDR (/48)"
 }
 
 variable "ext-network-0001-name" {
   type = string
-  default = "ext-network-0001.os-cloud-0001.vty-valentin-vty.net"
+  description = "Name of external network"
 }
-variable "ext-network-0001-pronettype" {
-  type = string
-  description = "Type of provider network (flat/vlan/local)"
-  default = "flat"
-}
-variable "ext-network-0001-prophynet" {
-  type = string
-  description = "Name of physical provider network (exp. br-provider)"
-  default = "br-provider"
-}
-variable "ext-network-0001-cidr-ipv4" {
-  type = string
-  description = "ext-network-0001 IPv4 CIDR"
-}
-variable "ext-network-0001-cidr-ipv6" {
-  type = string
-  description = "ext-network-0001 IPv6 CIDR"
-}
-
 
 provider "openstack" {
   user_name = var.os_user_name
@@ -83,7 +64,8 @@ provider "openstack" {
 
 resource "openstack_networking_network_v2" "network-0001" {
   name = var.network-0001-name
-  admin_state_up = "true"  
+  admin_state_up = "true"
+  shared = "false"
 }
 
 resource "openstack_networking_subnet_v2" "network-0001-subnet-0001" {
@@ -103,7 +85,7 @@ resource "openstack_networking_subnet_v2" "network-0001-subnet-0002" {
   ipv6_address_mode = "slaac"
   ipv6_ra_mode = "slaac"
   gateway_ip = cidrhost(cidrsubnet(var.network-0001-cidr-ipv6, 16, 1), 1)
-  enable_dhcp = false
+  enable_dhcp = true
 }
 
 resource "openstack_networking_subnet_v2" "network-0001-subnet-0003" {
@@ -126,290 +108,23 @@ resource "openstack_networking_subnet_v2" "network-0001-subnet-0004" {
   enable_dhcp = true
 }
 
-resource "openstack_networking_secgroup_v2" "sg-0001" {
-  name = "sg-0001"
-  description = "Allow all egress, Drop all ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0002" {
-  name = "sg-0002"
-  description = "Allow all egress, Allow all ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0003" {
-  name = "sg-0003"
-  description = "Allow all egress, Allow all ingress from network-0001, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0004" {
-  name = "sg-0004"
-  description = "Allow all egress, Allow ssh ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0005" {
-  name = "sg-0005"
-  description = "Allow no egress, Allow no ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0006" {
-  name = "sg-0006"
-  description = "Allow all egress, Allow ssh,http,https ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_secgroup_v2" "sg-0007" {
-  name = "sg-0007"
-  description = "Allow all egress, Allow https ingress, Dual-Stack"
-  delete_default_rules = true
-}
-
-resource "openstack_networking_network_v2" "ext-network-0001" {
+data "openstack_networking_network_v2" "ext-network-0001" {
   name = var.ext-network-0001-name
+}
+
+resource "openstack_networking_router_v2" "ext-net-rt-0001" {
+  name = "ext-net-rt-0001"
   admin_state_up = true
-  external = true
-  shared = true
-  provider_network_type = var.ext-network-0001-pronettype
-  provider_physical_network = var.ext-network-0001-prophynet
-}
-  
-resource "openstack_networking_subnet_v2" "ext-network-0001-subnet-0001" {
-  name = "subnet-0001.${var.ext-network-0001-name}"
-  network_id = openstack_networking_network_v2.ext-network-0001.id
-  cidr = cidrsubnet(var.ext-network-0001-ipv4, 8, 1)
-  ip_version = 4
-  gateway_ip = cidrhost(cidrsubnet(var.ext-network-0001-ipv4, 8, 1), 1)
-  enable_dhcp = false
+  external_network_id = data.openstack_networking_network_v2.ext-network-0001.id
+  # enable_snat = true
 }
 
-resource "openstack_networking_subnet_v2" "ext-network-0001-subnet-0002" {
-  name = "subnet-0002.${var.ext-network-0001-name}"
-  network_id = openstack_networking_network_v2.ext-network-0001.id
-  cidr = cidrsubnet(var.ext-network-0001-ipv6, 16, 1)
-  ip_version = 6
-  ipv6_address_mode = "slaac"
-  ipv6_ra_mode = "slaac"
-  gateway_ip = cidrhost(cidrsubnet(var.ext-network-0001-ipv6, 16, 1), 1)
-  enable_dhcp = false
+resource "openstack_networking_router_interface_v2" "ext-net-rt-0001-int-0001" {
+  router_id = openstack_networking_router_v2.ext-net-rt-0001.id
+  subnet_id = openstack_networking_subnet_v2.network-0001-subnet-0003.id
 }
 
-resource "openstack_networking_router_v2" "rt-0001" {
-  name = "rt-0001"
-  admin_state_up = true
-  external_network_id = openstack_networking_network_v2.ext-network-0001.id
-  enable_snat = true
-}
-
-resource "openstack_networking_router_interface_v2" "rt-0001-int-0001" {
-  router_id = openstack_networking_router_v2.rt-0001.id
-  subnet_id = openstack_networking_subnet_v2.network-0001-subnet-0003
-}
-
-resource "openstack_networking_router_interface_v2" "rt-0001-int-0002" {
-  router_id = openstack_networking_router_v2.rt-0001.id
-  subnet_id = openstack_networking_subnet_v2.network-0001-subnet-0004
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0001-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0001.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0001-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0001.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0002-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0002.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0002-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0002.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0002-rule-0003" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0002.id
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0002-rule-0004" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0002.id
-  remote_ip_prefix = "::/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0003-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0003.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0003-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0003.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0003-rule-0003" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0003.id
-  remote_ip_prefix = var.network-0001-cidr-ipv4
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0003-rule-0004" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0003.id
-  remote_ip_prefix = var.network-0001-cidr-ipv6
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0004-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0004.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0004-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0004.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0004-rule-0003" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0004.id
-  protocol = "tcp"
-  port_range_min = "22"
-  port_range_max = "22"
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0004-rule-0004" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0004.id
-  protocol = "tcp"
-  port_range_min = "22"
-  port_range_max = "22"
-  remote_ip_prefix = "::/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0003" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "22"
-  port_range_max = "22"
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0004" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "22"
-  port_range_max = "22"
-  remote_ip_prefix = "::/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0005" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "80"
-  port_range_max = "80"
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0006" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "80"
-  port_range_max = "80"
-  remote_ip_prefix = "::/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0007" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "443"
-  port_range_max = "443"
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0006-rule-0008" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0006.id
-  protocol = "tcp"
-  port_range_min = "443"
-  port_range_max = "443"
-  remote_ip_prefix = "::/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0007-rule-0001" {
-  direction = "egress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0007.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0007-rule-0002" {
-  direction = "egress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0007.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0007-rule-0003" {
-  direction = "ingress"
-  ethertype = "IPv4"
-  security_group_id = openstack_networking_secgroup_v2.sg-0007.id
-  protocol = "tcp"
-  port_range_min = "443"
-  port_range_max = "443"
-  remote_ip_prefix = "0.0.0.0/0"
-}
-
-resource "openstack_networking_secgroup_rule_v2" "sg-0007-rule-0004" {
-  direction = "ingress"
-  ethertype = "IPv6"
-  security_group_id = openstack_networking_secgroup_v2.sg-0007.id
-  protocol = "tcp"
-  port_range_min = "443"
-  port_range_max = "443"
-  remote_ip_prefix = "::/0"
+resource "openstack_networking_router_interface_v2" "ext-net-rt-0001-int-0002" {
+  router_id = openstack_networking_router_v2.ext-net-rt-0001.id
+  subnet_id = openstack_networking_subnet_v2.network-0001-subnet-0004.id
 }
