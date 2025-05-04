ea9f3170987b6e83c611d802236d60e04782ca7f
Valentin Binotto <git.8c400bab53f220f13efe0112c1ea2f96@v434project.com>
2025-05-04 00:11:30 +0000

    add os swift template

---
+++ templates/openstack/storage/swift/env.auto.tfvars
@@ -0,0 +1,3 @@
+swift-bucket-0001-name = ""
+swift-bucket-0001-readrole-0001 = ""
+swift-bucket-0001-writerole-0001 = ""
--- templates/openstack/storage/swift/main.tf
+++ templates/openstack/storage/swift/main.tf
@@ -1,1 +1,66 @@
 
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "3.0.0"
+    }
+  }
+}
+
+variable "os_user_name" { type = string }
+variable "os_tenant_name" { type = string }
+variable "os_user_password" {
+  type = string
+  sensitive = true
+}
+variable "os_auth_url" { type = string }
+variable "os_region" { type = string }
+variable "os_user_domain_name" { type = string }
+variable "os_project_domain_name" { type = string }
+variable "os_domain_name" { type = string }
+
+variable "swift-bucket-0001-name" {
+  type = string
+  default = "s3-0001.pr-0001.os-cloud-0001"
+}
+
+variable "swift-bucket-0001-readrole-0001" {
+  type = string
+  default = "rl-0001-s3-read-0001"
+}
+variable "swift-bucket-0001-writerole-0001" {
+  type = string
+  default = "rl-0001-s3-write-0001"
+}
+
+provider "openstack" {
+  user_name = var.os_user_name
+  tenant_name = var.os_tenant_name
+  password = var.os_user_password
+  auth_url = var.os_auth_url
+  region = var.os_region
+  user_domain_name = var.os_user_domain_name
+  project_domain_name = var.os_project_domain_name
+  domain_name = var.os_domain_name
+  enable_logging = true
+}
+
+
+#variable "cloud" {
+#  type = string
+#  default = "os-cloud-0001"
+#  sensitive = false
+#}
+
+#provider "openstack" {
+#  cloud = var.cloud
+#}
+
+
+resource "openstack_objectstorage_container_v1" {
+  name = var.swift-bucket-0001-name
+  versioning = true
+  container_read = var.swift-bucket-0001-readrole-0001
+  container_write = var.swift-bucket-0001-writerole-0001
+}
