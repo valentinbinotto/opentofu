5845ea9d602368cd72e7e7d3a8d655d5d8dd36eb
Valentin Binotto <git.8c400bab53f220f13efe0112c1ea2f96@v434project.com>
2025-05-04 21:17:06 +0000

    add aws opentofu template for vpc creation

---
+++ templates/aws/networking/vpc-only/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.97.0"
+    }
+  }
+}
+
+variable "aws_region" { type = string }
+
+variable "vpc-0001-cidr" {
+  type = string
+  default = "172.16.0.0/16"
+}
+
+variable "vpc-0001-name" {
+  type = string
+  default = "vpc-0001.aws-cloud-0001.vty-valentin-vty.net"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+resource "aws_vpc" "vpc-0001" {
+  cidr_block = var.vpc-0001-cidr
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc-0001-name
+  }
+}
